8ea40198fda2dc17e3416d0c68dac5651c9ad4f7
Felipe Freire <62055315+gandal-fe@users.noreply.github.com>
2022-11-10 02:03:14 -0300

    Update atv1.java

--- src/questionario1/atv1.java
+++ src/questionario1/atv1.java
@@ -1,7 +1,9 @@
 package questionario1;
 
 /* 
-  Altere o programa Java do slide 18 para que ele armazene a altura de três pessoas. Declare três variáveis e armazene nelas os seguintes valores: 1.58, 2.07 e 0.55. Por fim, realize o cálculo da média aritmética destas alturas, armazene o valor em uma outra variável e a apresente ao usuário.  A seguir o texto do código que criamos no slide 18.
+  Altere o programa Java do slide 18 para que ele armazene a altura de três pessoas. Declare três variáveis e armazene nelas os 
+  seguintes valores: 1.58, 2.07 e 0.55. Por fim, realize o cálculo da média aritmética destas alturas, armazene o valor em uma outra variável e
+  a apresente ao usuário.  A seguir o texto do código que criamos no slide 18.
 
 public class HelloWorldApp {
 
