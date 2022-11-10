e289a6604d7e860fcbca4d6ab2969b46dcc4e124
Felipe Freire <felipe.neves.freire@gmail.com>
2022-11-10 02:01:06 -0300

    first commit

---
+++ src/questionario1/atv1.java
@@ -0,0 +1,29 @@
+package questionario1;
+
+/* 
+  Altere o programa Java do slide 18 para que ele armazene a altura de três pessoas. Declare três variáveis e armazene nelas os seguintes valores: 1.58, 2.07 e 0.55. Por fim, realize o cálculo da média aritmética destas alturas, armazene o valor em uma outra variável e a apresente ao usuário.  A seguir o texto do código que criamos no slide 18.
+
+public class HelloWorldApp {
+
+      public static void main(String[] args) {
+            System.out.println("Meu primeiro programa em Java");
+      }
+}
+  
+ */
+
+public class atv1 {
+	
+	public static void main(String[] args) {
+      
+		double pessoa1 = 1.58, pessoa2 = 2.07, pessoa3 = 0.55;
+		
+		double media = (pessoa1 + pessoa2 + pessoa3)/3;
+		
+		System.out.print(media);
+		
+		
+    }
+	
+
+}
---
+++ src/questionario1/atv3.java
@@ -0,0 +1,43 @@
+package questionario1;
+
+/*
+ Crie um programa em Java no qual o usuário deve entrar com a operação a ser realizada (soma '+', subtração '-' ou multiplicação '*') e dois números inteiros. O programa deverá utilizar o comando switch para escolher entre as operações e mostrar o resultado da operação.
+
+Saída esperada 1:
+
+Entre com a operação a ser realizada (+, - ou *):
+
+Entre com o primeiro número:
+
+Entre com o segundo número:
+
+O resultado da operação é: <RESULTADO>
+
+Saída esperada 2:
+
+Entre com a operação a ser realizada (+, - ou *):
+
+Entre com o primeiro número:
+
+Entre com o segundo número:
+
+Operação inválida
+ */
+
+import java.util.Scanner;
+
+public class atv3 {
+	
+	public static void main(String[] args) {
+		
+		char operacao;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com a operação a ser realizada (+, - ou *):");
+		operacao = scan.next().charAt(0);
+		
+		System.out.println(operacao);
+	}
+
+}
